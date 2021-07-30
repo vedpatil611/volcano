5b1e0abf417dca9bf66674fc787b5124976cea47
vedpatil611 <vedpatil611@gmail.com>
2021-07-30 19:44:19 +0530

    More vertex inputs in shader

--- volcano/shaders/shader.fs.glsl
+++ volcano/shaders/shader.fs.glsl
@@ -1,9 +1,11 @@
 #version 450
 #pragma shader_stage(fragment)
 
-layout(location = 0) out vec4 outColour;
+layout (location = 0) in vec4 v_color;
+
+layout (location = 0) out vec4 outColour;
 
 void main()
 {
-    outColour = vec4(1.0f, 0.0f, 0.0f, 1.0f);
+    outColour = v_color;
 }
--- volcano/shaders/shader.vs.glsl
+++ volcano/shaders/shader.vs.glsl
@@ -2,8 +2,12 @@
 #pragma shader_stage(vertex)
 
 layout (location = 0) in vec3 position;
+layout (location = 1) in vec4 color;
+
+layout (location = 0) out vec4 v_color;
 
 void main()
 {
     gl_Position = vec4(position, 1.0f);
+    v_color = color;
 }
