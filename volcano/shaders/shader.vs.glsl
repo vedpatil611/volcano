73b1197a45fcc0929ebd67456c3641c391e87741
vedpatil611 <vedpatil611@gmail.com>
2021-09-03 11:06:20 +0530

    Split bindings for view proj and model matrix

--- volcano/shaders/shader.vs.glsl
+++ volcano/shaders/shader.vs.glsl
@@ -5,16 +5,19 @@ layout (location = 0) in vec3 position;
 layout (location = 1) in vec4 color;
 
 //hidden (set = 0) <- descriptor set
-layout (binding = 0) uniform MVP {
+layout (binding = 0) uniform UBOViewProj {
     mat4 proj;
     mat4 view;
+} uboViewProj;
+
+layout (binding = 1) uniform UBOModel {
     mat4 model;
-} mvp;
+} uboModel;
 
 layout (location = 0) out vec4 v_color;
 
 void main()
 {
-    gl_Position = mvp.proj * mvp.view * mvp.model * vec4(position, 1.0f);
+    gl_Position = uboViewProj.proj * uboViewProj.view * uboModel.model * vec4(position, 1.0f);
     v_color = color;
 }
